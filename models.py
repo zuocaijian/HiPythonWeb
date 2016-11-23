36bfa247511cbf4a073c1126c0d578bc6e88646b
zcj <zcj@clever-rock.com>
2016-11-24 00:38:34 +0800

    day3

---
+++ models.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+"""
+实体类
+"""
+
+__author__ = 'zcj'
+
+from orm import Model, StringField, IntegerField
+
+class User(Model):
+    __table__ = 'users'
+
+    id = IntegerField(primary_key=True)
+    name = StringField()
